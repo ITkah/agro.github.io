eed3e96579e3bc35f5e84f7588e8b1c7eb3cb24b
ITkah <ilatkacenko132@gmail.com>
2020-04-18 14:44:12 +0300

    career page

--- js/common.js
+++ js/common.js
@@ -13,6 +13,25 @@ $(document).ready(function() {
           .addClass('active').siblings().removeClass('active')
           .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
+
+    document.querySelector("html").classList.add('js');
+
+    var fileInput  = document.querySelector( ".input-file" ),  
+        button     = document.querySelector( ".input-file-trigger" ),
+        the_return = document.querySelector(".file-return");
+        
+    button.addEventListener( "keydown", function( event ) {  
+        if ( event.keyCode == 13 || event.keyCode == 32 ) {  
+            fileInput.focus();  
+        }  
+    });
+    button.addEventListener( "click", function( event ) {
+    fileInput.focus();
+    return false;
+    });  
+    fileInput.addEventListener( "change", function( event ) {  
+        the_return.innerHTML = this.value;  
+    });  
     
     $('.review-slider').slick({
         infinite: true,
