0d471a9c79d9449d59bd4856d1ea7374224643ff
ITkah <ilatkacenko132@gmail.com>
2020-04-18 15:35:40 +0300

    product

--- js/common.js
+++ js/common.js
@@ -1,44 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $(".menu-call").on("click", function(){
+    $(".menu-call").on("click", function () {
         $(".mob-menu").addClass("mob-menu-active");
     });
-    
-    $(".close").on("click", function(){
+
+    $(".close").on("click", function () {
         $(".mob-menu").removeClass("mob-menu-active");
     });
 
-    $('div.tabs__caption').on('click', 'p:not(.active)', function() {
+    $('div.tabs__caption').on('click', 'p:not(.active)', function () {
         $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
 
     document.querySelector("html").classList.add('js');
 
-    var fileInput  = document.querySelector( ".input-file" ),  
-        button     = document.querySelector( ".input-file-trigger" ),
+    var fileInput = document.querySelector(".input-file"),
+        button = document.querySelector(".input-file-trigger"),
         the_return = document.querySelector(".file-return");
-        
-    button.addEventListener( "keydown", function( event ) {  
-        if ( event.keyCode == 13 || event.keyCode == 32 ) {  
-            fileInput.focus();  
-        }  
+
+    button.addEventListener("keydown", function (event) {
+        if (event.keyCode == 13 || event.keyCode == 32) {
+            fileInput.focus();
+        }
     });
-    button.addEventListener( "click", function( event ) {
-    fileInput.focus();
-    return false;
-    });  
-    fileInput.addEventListener( "change", function( event ) {  
-        the_return.innerHTML = this.value;  
-    });  
+
+    button.addEventListener("click", function (event) {
+        fileInput.focus();
+        return false;
+    });
     
-    $('.review-slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
+    fileInput.addEventListener("change", function (event) {
+        the_return.innerHTML = this.value;
     });
+
 
 });
